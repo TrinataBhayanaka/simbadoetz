aa65bf323fa385d3ed036ef7febf9a3e68d1c335
ilmawan <ilmawan.muharam@gmail.com>
2014-10-08 14:55:46 +0700

    percobaan

--- index.php
+++ index.php
@@ -63,7 +63,7 @@ $USERAUTH->check_sys_log();
 					  <li class="active">Flat Screens</li> -->
 					  <?php SignInOut();?>
 					</ul>
-					<div class="title">Headline News</div>
+					<div class="title">Headline News coba edit aja</div>
 					<div class="breadcrumb">
 						It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
  
