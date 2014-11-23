8d1f5b4da663f4aa77471c9fdbc89391e480a1e6
andreassbayu <andreass.bayu@gmail.com>
2014-11-24 00:54:46 +0700

    rev dropselect satker

--- function/api/satker.php
+++ function/api/satker.php
@@ -3,6 +3,7 @@ include "../../config/database.php";
 open_connection();  
 		
 	$term = $_GET['term'];
+	if($term=="") $term = "dummybabyyeah";
 	$cond = "KodeSatker is NOT NULL AND KodeUnit is NOT NULL AND Gudang is NOT NULL AND Kd_Ruang is NULL";
 
 	$sql = mysql_query("SELECT * FROM satker WHERE kode LIKE '{$term}%' ORDER BY Satker_ID ASC");	
