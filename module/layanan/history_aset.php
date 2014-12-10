5cd6c168b3f0eba865dbfddd5262c9a759ac8bcd
pukiscuy <pukiscuy@gmail.com>
2014-12-11 06:36:45 +0700

    template histoy aset

---
+++ module/layanan/history_aset.php
@@ -0,0 +1,126 @@
+<?php
+include "../../config/config.php";
+
+?>
+
+<?php
+	include"$path/meta.php";
+	include"$path/header.php";
+	include"$path/menu.php";
+	
+?>
+	
+	<!-- End Sql -->
+	<section id="main">
+		<ul class="breadcrumb">
+			  <li><a href="#"><i class="fa fa-home fa-2x"></i>  Home</a> <span class="divider"><b>&raquo;</b></span></li>
+			  <li><a href="#">Layanan Aset</a><span class="divider"><b>&raquo;</b></span></li>
+			  <li class="active">History Aset</li>
+			  <?php SignInOut();?>
+			</ul>
+			<div class="breadcrumb">
+				<div class="title">Layanan Aset</div>
+				<div class="subtitle">History Aset</div>
+			</div>	
+
+		
+
+		<section class="formLegend">
+			
+			<div class="detailLeft">
+						
+						<ul>
+							<li>
+								<span class="labelInfo">Kode Barang</span>
+								<input type="text" value="<?=$kontrak[0]['noKontrak']?>" disabled/>
+							</li>
+							<li>
+								<span class="labelInfo">Kode Lokasi</span>
+								<input type="text" value="<?=$kontrak[0]['tglKontrak']?>" disabled/>
+							</li>
+
+							<li>
+								<span class="labelInfo">Nilai Perolehan</span>
+								<input type="text" value="<?=$kontrak[0]['tglKontrak']?>" disabled/>
+							</li>
+						</ul>
+							
+					</div>
+			<div class="detailRight">
+						
+						<ul>
+							<li>
+								<span class="labelInfo">Nama Barang</span>
+								<input type="text" value="<?=number_format($kontrak[0]['nilai'])?>" disabled/>
+							</li>
+							<li>
+								<span  class="labelInfo">SKPD</span>
+								<input type="text" value="<?=isset($sumTotal) ? number_format($sumTotal['total']) : '0'?>" disabled/>
+							</li>
+						</ul>
+							
+					</div>
+			<div style="height:5px;width:100%;clear:both"></div>
+
+			
+				
+			<div id="demo">
+			<table cellpadding="0" cellspacing="0" border="0" class="display" id="example">
+				<thead>
+					<tr>
+						<th>No</th>
+						<th>Tanggal</th>
+						<th>Keterangan</th>
+						<th>SKPD</th>
+						<th>Status</th>
+					</tr>
+				</thead>
+				<tbody>
+				<?php
+					// if($rKontrak){
+					// 	$i = 1;
+					// 	foreach ($rKontrak as $key => $value) {
+							
+						
+				?>
+					<tr class="gradeA">
+						<td><?=$i?></td>
+						<td><?=$value['kodeKelompok']?></td>
+						<td><?=$value['uraian']?></td>
+						<td class="center"><?=$value['Kuantitas']?></td>
+						<td class="center">
+						<?php
+							// if($kontrak[0]['n_status'] != 1){
+						?>
+						<!--<a href="kontrak_rincianubah.php?id=<?=$value['Aset_ID']?>&tmpthis=<?=$_GET['id']?>" class="btn btn-warning btn-small" ><i class="icon-pencil icon-white"></i>&nbsp;Edit</a>-->
+						<!-- <a href="kontrak_rincianhapus.php?id=<?=$value['kodeKelompok']?>&idLok=<?=$value['kodeLokasi']?>&tmpthis=<?=$_GET['id']?>" class="btn btn-danger btn-small" onclick="return confirm('Hapus Aset?')"><i class="icon-trash icon-white"></i>&nbsp;Hapus</a> -->
+						
+						<?php
+						// } else {
+							echo "<span class='label label-Success'>Berhasil</span>";
+						// }
+						?>
+						</td>
+					</tr>
+				<?php
+					// 	$i++;
+					// 	}
+					// }	
+				?>
+				</tbody>
+				<tfoot>
+					<tr>
+						<th colspan="5">&nbsp;</th>
+					</tr>
+				</tfoot>
+			</table>
+			</div>
+			<div class="spacer"></div>
+			    
+		</section> 
+		       
+	</section>
+	
+<?php
+	include"$path/footer.php";
+?>
