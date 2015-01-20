e81acc2f86ab31de95e74b521c723e0e6048bafe
pukiscuy <pukiscuy@gmail.com>
2015-01-20 14:13:27 +0700

    psb

--- module/penghapusan/penetapan_penghapusan_filter.php
+++ module/penghapusan/penetapan_penghapusan_filter.php
@@ -85,7 +85,7 @@ $USERAUTH->FrontEnd_check_akses_menu($menu_id,$SessionUser);
 		</div>
 		<section class="formLegend">
 			
-			<form name="form" method="POST" action="<?php echo "$url_rewrite"; ?>/module/penghapusan/penetapan_penghapusan_daftar_isi.php?pid=1" onsubmit="return requiredFilter(false)">
+			<form name="form" method="POST" action="<?php echo "$url_rewrite"; ?>/module/penghapusan/penetapan_penghapusan_daftar_isi.php?pid=1" onsubmit="return requiredFilter(true,false)">
 			<ul>
 							<!-- <li>
 								<span class="span2">Tanggal awal</span>
