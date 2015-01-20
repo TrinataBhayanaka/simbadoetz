2190e8f50e521b53374b3ec6bd8090ca83741236
andreassbayu <andreass.bayu@gmail.com>
2015-01-20 23:55:10 +0700

    rev ruangan add tahun

--- function/api/ruang.php
+++ function/api/ruang.php
@@ -3,10 +3,11 @@ include "../../config/database.php";
 open_connection();  
 		
 	$term = $_GET['term'];
+	$tahun = $_GET['tahun'];
 	if($term=="") $term = "dummybabyyeah";
 	$cond = "Kd_Ruang IS NOT NULL";
 
-	$sql = mysql_query("SELECT * FROM satker WHERE {$cond} AND  kode LIKE '{$term}%' ORDER BY Satker_ID ASC");	
+	$sql = mysql_query("SELECT * FROM satker WHERE {$cond} AND  kode LIKE '{$term}%' AND Tahun = '{$tahun}' ORDER BY Satker_ID ASC");	
 	
 	while ($row = mysql_fetch_assoc($sql)){
 				$data[] = $row;
