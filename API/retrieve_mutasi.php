e78b420b8161272864122264ceadc51d9dddedfc
Ovan Pulu <ovan89@gmail.com>
2015-01-27 13:43:35 +0700

    filter diatas 2014

--- API/retrieve_mutasi.php
+++ API/retrieve_mutasi.php
@@ -277,16 +277,21 @@ class RETRIEVE_MUTASI extends RETRIEVE{
                     $sqlSelect = array(
                         'table'=>"mutasi AS m, satker AS s",
                         'field'=>"m.*, s.NamaSatker",
-                        'condition'=>"Mutasi_ID = '{$value[Mutasi_ID]}' AND s.Kd_Ruang IS NULL ORDER BY m.TglSKKDH DESC",
+                        'condition'=>" Mutasi_ID = '{$value[Mutasi_ID]}' AND s.Kd_Ruang IS NULL AND m.TglSKKDH > '2014-01-01' AND m.TglSKKDH IS NOT NULL ORDER BY m.TglSKKDH DESC",
                         'joinmethod' => 'LEFT JOIN',
                         'join'=>'m.SatkerTujuan = s.kode',
                         );
 
-                    $res[] = $this->db->lazyQuery($sqlSelect,$debug);
-                    $res[$key][0]['SatkerAwal'] = $value['SatkerAwal'];
-                    $res[$key][0]['NamaSatkerAwal'] = $value['NamaSatkerAwal'];
-                    $res[$key][0]['NamaSatkerAwalAset'] = $value['NamaSatkerAwalAset'];
-                    $res[$key][0]['Jumlah'] = $value['Jumlah'];
+                    $tmpRes = $this->db->lazyQuery($sqlSelect,$debug);
+                    if ($tmpRes){
+
+                        $res[] = $tmpRes;
+                        $res[$key][0]['SatkerAwal'] = $value['SatkerAwal'];
+                        $res[$key][0]['NamaSatkerAwal'] = $value['NamaSatkerAwal'];
+                        $res[$key][0]['NamaSatkerAwalAset'] = $value['NamaSatkerAwalAset'];
+                        $res[$key][0]['Jumlah'] = $value['Jumlah'];
+                    }
+                    
                 }
                 
                 // pr($res);
