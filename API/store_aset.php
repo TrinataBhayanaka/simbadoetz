73f99340001d329728abdbaa07635f9b17bcaf7b
andreassbayu <andreass.bayu@gmail.com>
2015-02-17 12:29:45 +0700

    inventarisasi

--- API/store_aset.php
+++ API/store_aset.php
@@ -2425,6 +2425,7 @@ $id_kapitalisasi_aset=  get_auto_increment("KapitalisasiAset");
               while ($dataAset = mysql_fetch_assoc($sqlquery)){
                       $kib = $dataAset;
                   }
+              $kib['TglPerubahan'] = $kib['TglPerolehan'];    
               $kib['changeDate'] = date("Y-m-d");
               $kib['action'] = 1;
               $kib['operator'] = $_SESSION['ses_uoperatorid'];
