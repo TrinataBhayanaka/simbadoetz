ea70e079876755f87df1a84d6c63b82b7ca2396d
Ovan Pulu <ovan89@gmail.com>
2015-02-27 15:33:06 +0700

    ilangin log penggunaan

--- api_list/penggunaan-helper.php
+++ api_list/penggunaan-helper.php
@@ -273,6 +273,7 @@ class PENGGUNAAN extends DB{
 	           	} 
             }
 
+            /*
             $sleep = 1;
             
             foreach ($asetid as $key => $value) {
@@ -285,6 +286,7 @@ class PENGGUNAAN extends DB{
 	           		$sleep = 1;	
 	           	} 
             }
+            */
         }else{
             logFile('gagal log penggunaan');
         }
@@ -329,7 +331,7 @@ class PENGGUNAAN extends DB{
 
 $run = new PENGGUNAAN;
 $getGUID = $run->getGUID();
-$kontrak = "050/374/XI/2014";
+$kontrak = "importing-ug/2";
 
 if ($getGUID){
 	
