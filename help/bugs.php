22fd4b8160ba3810b9bf8b6715f106fe584ad3a6
Ovan Pulu <ovan89@gmail.com>
2015-10-01 15:32:53 +0700

    bugs

--- help/bugs.php
+++ help/bugs.php
@@ -5,7 +5,7 @@ mysql_connect('192.168.254.52','remote','margonda100');
 mysql_select_db('simbada_latihan');
 
 if (isset($_POST['submit'])){
-	$sql = "INSERT INTO help (skpd, nama, masalah) VALUES ('{$_POST['skpd']}','{$_POST['nama']}','{$_POST['masalah']}')";
+	$sql = "INSERT INTO help (skpd, nama, masalah, nourut) VALUES ('{$_POST['skpd']}','{$_POST['nama']}','{$_POST['masalah']}','{$_POST['nourut']}')";
 	$res = mysql_query($sql);
 	if ($res){
 		echo '<script type=text/javascript>alert("Terima Kasih atas partisipasinya untuk SIMBADA");window.location.href="http://simbada.pekalongankota.go.id/latihan";</script>';
@@ -18,5 +18,6 @@ if (isset($_POST['submit'])){
 	SKPD <input type="text" name="skpd">
 	Nama Operator <input type="text" name="nama">
 	Permasalahan <input type="text" name="masalah">
+	No urut <input type="text" name="nourut">
 	<input type="submit" name="submit">
 </form>
