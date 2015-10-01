2fd0f180b6fce321d6b099effb2d578ce202d4d0
Ovan Pulu <ovan89@gmail.com>
2015-10-01 15:20:57 +0700

    bugs

--- help/bugs.php
+++ help/bugs.php
@@ -4,11 +4,14 @@
 mysql_connect('192.168.254.52','remote','margonda100');  
 mysql_select_db('simbada_latihan');
 
-$sql = "INSERT INTO help (skpd, nama, masalah) VALUES ('{$_POST['skpd']}','{$_POST['nama']}','{$_POST['masalah']}')";
-$res = mysql_query($sql);
-if ($res){
-	echo '<script type=text/javascript>alert("Terima Kasih atas partisipasinya untuk SIMBADA");window.location.href="http://simbada.pekalongankota.go.id/latihan";</script>';
+if (isset($_POST['submit'])){
+	$sql = "INSERT INTO help (skpd, nama, masalah) VALUES ('{$_POST['skpd']}','{$_POST['nama']}','{$_POST['masalah']}')";
+	$res = mysql_query($sql);
+	if ($res){
+		echo '<script type=text/javascript>alert("Terima Kasih atas partisipasinya untuk SIMBADA");window.location.href="http://simbada.pekalongankota.go.id/latihan";</script>';
+	}
 }
+
 ?>
 <form method="post" action="">
 
