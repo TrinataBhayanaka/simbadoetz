b63f926ede21025cb861af70f55bd0f1ebfd322a
andre.cun <andre.hadiyono@gmail.com>
2016-02-06 02:22:15 +0700

    update commit untuk database

--- function/class/class_db.php
+++ function/class/class_db.php
@@ -155,9 +155,10 @@ class DB
 	}
 	
 	function commit($dbuse=0)
-	{
+	{   
 		$command = "COMMIT;";
 		$result = $this->query($command) or die ($this->error('commit failed'));
+                $this->autocommit(1);
 		// mysql_commit();
 	}
 	
@@ -165,6 +166,7 @@ class DB
 	{
 		$command = "ROLLBACK;";
 		$result = $this->query($command) or die ($this->error('rollback failed'));
+                $this->autocommit(1);
 		// if (!$this->link){
 			// $this->link = $this->open_connection(0);
 		// }
