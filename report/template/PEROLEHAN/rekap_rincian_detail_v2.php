e3885dd3bb10020bc098528b032e18863e430a4b
andre.cun <andre.hadiyono@gmail.com>
2016-03-22 00:16:16 +0700

    rincian detail -->testing

--- report/template/PEROLEHAN/rekap_rincian_detail_v2.php
+++ report/template/PEROLEHAN/rekap_rincian_detail_v2.php
@@ -940,7 +940,7 @@ else
 
 function group_data($data_awal_perolehan,$data_akhir_perolehan,$data_hapus_awal){
     
-    
+    //tes
  $data_awal = array();
 
 foreach($data_awal_perolehan as $arg)
@@ -979,8 +979,7 @@ $data_hapus_tmp = array();
 
 foreach($data_hapus_awal as $arg)
 {
-   
-    $data_hapus_tmp[$arg['kelompok'].'.'.$arg['Tahun'].'-'.$arg['noRegister'].'-'.$arg['Aset_ID']]['Kelompok']+= $arg['kelompok'];
+     $data_hapus_tmp[$arg['kelompok'].'.'.$arg['Tahun'].'-'.$arg['noRegister'].'-'.$arg['Aset_ID']]['Kelompok']+= $arg['kelompok'];
     $data_hapus_tmp[$arg['kelompok'].'.'.$arg['Tahun'].'-'.$arg['noRegister'].'-'.$arg['Aset_ID']]['Uraian']+= $arg['Uraian'];
     $data_hapus_tmp[$arg['kelompok'].'.'.$arg['Tahun'].'-'.$arg['noRegister'].'-'.$arg['Aset_ID']]['nilai']+= $arg['nilai'];
     $data_hapus_tmp[$arg['kelompok'].'.'.$arg['Tahun'].'-'.$arg['noRegister'].'-'.$arg['Aset_ID']]['PP']+= $arg['PP'];
