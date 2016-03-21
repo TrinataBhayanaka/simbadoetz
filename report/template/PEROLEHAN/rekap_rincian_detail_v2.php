caef0ba4fe871ca4b3ff647c38a8c21ffb46f224
andre.cun <andre.hadiyono@gmail.com>
2016-03-22 00:08:29 +0700

    rincian detail -->revisi bp tahun berjalan untuk aset yang di terima tahun berjalan

--- report/template/PEROLEHAN/rekap_rincian_detail_v2.php
+++ report/template/PEROLEHAN/rekap_rincian_detail_v2.php
@@ -1462,6 +1462,8 @@ function get_uraian($kode,$level){
 
 function get_akumulasi_sblm($Aset_ID,$TahunPenyusutan,$kelompok){
     $gol=  explode(".", $kelompok);
+    echo "$gol";
+    exit();
     switch ($gol[0]) {
         case "01":
             $nama_log="log_tanah";
