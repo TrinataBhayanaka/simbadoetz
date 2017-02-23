6c81e2b24fb5a514a53e3044d44012dde6ce1c0a
Andreas Hadiyono <andre.hadiyono@gmail.com>
2017-02-23 15:10:28 +0700

    rekap kk

--- report/template/PEROLEHAN/fungsi_rekap_kk.php
+++ report/template/PEROLEHAN/fungsi_rekap_kk.php
@@ -271,7 +271,7 @@ function history_log($kode, $gol, $ps, $tglawalperolehan, $tglakhirperolehan, $T
             echo "<br/>masukk===$kondisi_next== $kondisi ==$log_id ==$Aset_ID==$final_gol ";
         }
         $status_kondisi = 0;
-        if(($kondisi_next == "1" || $kondisi_next == "2 ") and $kondisi == 3) {
+        if(($kondisi_next == "1" || $kondisi_next == "2 ") and $kondisi == "3") {
             $status_kondisi = 1;
         }
 
@@ -279,7 +279,7 @@ function history_log($kode, $gol, $ps, $tglawalperolehan, $tglakhirperolehan, $T
             $status_kondisi = -7;
         }
 
-        if(($kondisi_next = "1" || $kondisi_next = "2") || ($kondisi = "1" || $kondisi = "2")) {
+        if(($kondisi_next == "1" || $kondisi_next == "2") || ($kondisi == "1" || $kondisi == "2")) {
             $status_kondisi = 0;
         }
         if( $Kd_Riwayat == "1")
