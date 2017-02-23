3701bce7ca07b09a9b68141edbc22b3cb7aefd86
Andreas Hadiyono <andre.hadiyono@gmail.com>
2017-02-23 20:40:53 +0700

    rekap kk

--- report/template/PEROLEHAN/fungsi_rekap_kk.php
+++ report/template/PEROLEHAN/fungsi_rekap_kk.php
@@ -268,17 +268,18 @@ function history_log($kode, $gol, $ps, $tglawalperolehan, $tglakhirperolehan, $T
         $kondisi_next = 0;
         if($Kd_Riwayat == "0" || $Kd_Riwayat == "1") {
             list($Status_Validasi_Barang, $StatusValidasi, $StatusTampil, $TglPembukuan, $kondisi_next) = get_log_status_validasi ($tabel_log, $log_id, $Aset_ID, $final_gol);
-            echo "<br/>masukk===$kondisi_next== $kondisi ==$log_id ==$Aset_ID==$final_gol ";
+            echo "<br/>masukk==kondisi_next=$kondisi_next=kondisi-skrg= $kondisi ==$log_id ==$Aset_ID==$final_gol ";
         }
         $status_kondisi = 0;
         if(($kondisi_next == "1" || $kondisi_next == "2 ") && $kondisi == "3") {
-            echo "masuk <br>";
+            echo "masuk kondisi tambah <br>";
             $status_kondisi = 1;
         }else if($kondisi_next == "3" && ($kondisi == "1" || $kondisi == "2")) {
-            echo "==masuk <br>";
+            echo "masuk kondisi tambah<br>";
             $status_kondisi = -7;
         }else if(($kondisi_next == "1" || $kondisi_next == "2") && ($kondisi == "1" || $kondisi == "2")) {
             $status_kondisi = 0;
+            echo "tidak masuk kondisi apapun <br/>";
         }
         if( $Kd_Riwayat == "1")
             echo "<br/>$Aset_ID==$status_kondisi";
