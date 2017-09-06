d320e3ee94f4967cd853ec2f5a796133a8578c57
ilmawan <ilmawan.muharam@gmail.com>
2017-09-06 08:42:14 +0700

    update fungsi tambah ruangan kir

--- function/api/addruang.php
+++ function/api/addruang.php
@@ -6,7 +6,7 @@ open_connection();
 	$ruangan = $_POST['ruangan'];
 	$tahun = $_POST['tahun'];
 
-	$sql = mysql_query("SELECT MAX(Satker_ID) AS id FROM satker WHERE kode LIKE '{$kodesatker}%' AND Tahun = '{$tahun}'");
+	$sql = mysql_query("SELECT MAX(Kd_Ruang) AS Kd_Ruang,Satker_ID FROM satker WHERE kode LIKE '{$kodesatker}%' AND Tahun = '{$tahun}'");
 	
 	
 	while ($row = mysql_fetch_assoc($sql)){
@@ -14,13 +14,13 @@ open_connection();
 			}
 	if($data){
 
-		$sql = mysql_query("SELECT Kd_Ruang FROM satker WHERE Satker_ID = '{$data['id']}' LIMIT 1");
+		//$sql = mysql_query("SELECT Kd_Ruang FROM satker WHERE Satker_ID = '{$data['Satker_ID']}' LIMIT 1");
 	
 	
-		while ($row = mysql_fetch_assoc($sql)){
-					$kdruang = $row;
-				}
-		$kd_ruang = str_replace(' ', '', $kdruang['Kd_Ruang']);
+		//while ($row = mysql_fetch_assoc($sql)){
+					//$kdruang = $row;
+				//}
+		$kd_ruang = str_replace(' ', '', $data['Kd_Ruang']);
 		$code = explode(".", $kodesatker);
 		$satker['KodeSektor'] = $code[0];
 		$satker['kodeSatker'] = $code[0].".".$code[1];
