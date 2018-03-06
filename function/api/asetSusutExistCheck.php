003f88085be774bb218e962abb04e83949b41644
andre.hadiyono <andre.hadiyono@gmail.com>
2018-03-06 22:43:33 +0700

    filter penyusutan

--- function/api/asetSusutExistCheck.php
+++ function/api/asetSusutExistCheck.php
@@ -5,7 +5,7 @@ open_connection();
 	$satker = $_POST['satker'];
 	$kode = $_POST['kode'];
 	$tahun = $_POST['tahun'];
-	$sql = mysql_query("SELECT COUNT(*) as total FROM penyusutan_tahun WHERE kodeSatker ='{$satker}' AND KelompokAset like '{$kode}%' AND Tahun = '{$tahun}'");
+	$sql = mysql_query("SELECT COUNT(*) as total FROM 	penyusutan_tahun_berjalan WHERE kodeSatker ='{$satker}' AND KelompokAset like '{$kode}%' AND Tahun = '{$tahun}'");
 	while ($row = mysql_fetch_assoc($sql)){
 		$list = $row['total'];
 	}
