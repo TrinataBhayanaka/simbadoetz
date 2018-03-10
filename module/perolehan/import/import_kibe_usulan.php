8a947c26b6791267701ef88307b9da60bf74a2f2
ilmawan <ilmawan.muharam@gmail.com>
2018-03-10 13:17:13 +0700

    update

--- module/perolehan/import/import_kibe_usulan.php
+++ module/perolehan/import/import_kibe_usulan.php
@@ -8,6 +8,9 @@ print_r($CONFIG);
 echo "</pre>";*/
 $link = mysqli_connect($CONFIG['default']['db_host'],$CONFIG['default']['db_user'],$CONFIG['default']['db_pass'],$CONFIG['default']['db_name']) or die("Error " . mysqli_error($link)); 
 
+//start process
+$time_start = microtime(true); 
+
 $idKontrak = $argv[1];
 echo "idKontrak : ".$idKontrak."\n\n";
 
@@ -58,10 +61,16 @@ echo "SATKER AWAL : ".$rowKontrak['kodeSatker']."\n\n";
 			//create dokumen mutasi
 			$sqlCreateMutasi = "INSERT INTO mutasi(SatkerUsul, SatkerTujuan, UserNm, TglSKKDH, NoSKKDH, Keterangan,FixMutasi,Usulan_ID) VALUES  ('{$SatkerUsul}','{$SatkerTujuan}','{$UserNm}','{$TglUpdate}','{$NoUsulan}','{$KetUsulan}','0','{$IDUsulan}')" or die("Error in the consult.." . mysqli_error($link));
 		    //echo "sqlCreateMutasi : ".$sqlCreateMutasi."\n\n";
-		    			
+
 			$execCreateMutasi = $link->query($sqlCreateMutasi);
 			$IDPenetapanMTS = mysqli_insert_id($link);
 			echo "IDPenetapanMTS : ".$IDPenetapanMTS."\n\n";
+
+			//UPDATE FixMutasi PENETAPAN MUTASI DI MUTASI(flag proses input data) 
+			$queryUPD = "UPDATE mutasi
+				SET FixMutasi = '3'
+			where Mutasi_ID = '{$IDPenetapanMTS}' " or die("Error in the consult.." . mysqli_error($link));
+			$execUPD = $link->query($queryUPD);
 
 			//select data aset by kodesatker and nokontrak
 			$DataAset = array();
@@ -79,6 +88,7 @@ echo "SATKER AWAL : ".$rowKontrak['kodeSatker']."\n\n";
 					# code...
 					//insert usulanaset 
 					$Aset_ID = $value2['Aset_ID'];
+					$TpAst = $value2['kodeKelompok'];
 					$listAsset[]=$Aset_ID;
 					//echo $no."."."Aset_ID : ".$Aset_ID."\n\n"; 
 					$sqlCreateUsulanAset = "INSERT INTO usulanaset(Usulan_ID, Aset_ID, Jenis_Usulan) 
@@ -88,18 +98,14 @@ echo "SATKER AWAL : ".$rowKontrak['kodeSatker']."\n\n";
 					$execCreateUsulanAset = $link->query($sqlCreateUsulanAset);
 
 					//NamaSatker
-					$sqlSatker = "SELECT NamaSatker FROM satker where kode = '{$SatkerUsul}'";
+					$sqlSatker = "SELECT NamaSatker FROM satker where kode = '{$SatkerUsul}' AND Kd_Ruang IS null";
 					//echo "sqlSatker : ".$sqlSatker."\n\n";	
 					$resultSatker = $link->query($sqlSatker);
 					$rowSatker = mysqli_fetch_assoc($resultSatker); 
 					$NamaSatkerAwal = $rowSatker['NamaSatker'];
 
 					//NomorRegAwal
-					$sqlAset = "SELECT * FROM aset where Aset_ID = '{$Aset_ID}'";
-					//echo "sqlAset : ".$sqlAset."\n\n";	
-					$resultAset = $link->query($sqlAset);
-					$detailAset = mysqli_fetch_assoc($resultAset);
-					$NomorRegAwal = $detailAset['noRegister'];
+					$NomorRegAwal = $value2['noRegister'];
 
 					//insert mutasi aset			
 					$fieldPA = "Mutasi_ID,Aset_ID,Status,NamaSatkerAwal,SatkerAwal,SatkerTujuan,NomorRegAwal";
@@ -107,7 +113,163 @@ echo "SATKER AWAL : ".$rowKontrak['kodeSatker']."\n\n";
 				    $queryPA = "INSERT INTO mutasiaset ({$fieldPA}) VALUES ({$valuePA})" or die("Error in the consult.." . mysqli_error($link));	
 				   	//echo "queryPA : ".$queryPA."\n\n";
 				    $execPA = $link->query($queryPA);	
-				
+
+				    //get param table
+					$exp = explode('.', $TpAst);
+					if($exp['0'] == '01'){
+						$table = "tanah";
+						$tableLog = "log_tanah";
+					}elseif ($exp['0'] == '02') {
+						$table = "mesin";
+						$tableLog = "log_mesin";
+					}elseif ($exp['0'] == '03') {
+						$table = "bangunan";
+						$tableLog = "log_bangunan";
+					}elseif ($exp['0'] == '04') {
+						$table = "jaringan";
+						$tableLog = "log_jaringan";
+					}elseif ($exp['0'] == '05') {
+						$table = "asetlain";
+						$tableLog = "log_asetlain";
+					}elseif ($exp['0'] == '06') {
+						$table = "kdp";
+						$tableLog = "log_kdp";
+					}
+
+					$kodeSatker = explode('.', $SatkerTujuan);
+				    $kodePemilik = substr($value2['kodeLokasi'], 0,3);
+					$kodeLokasi = $kodePemilik."11.33.".$kodeSatker[0].".".$kodeSatker[1].".".substr($value2['Tahun'],-2).".".$kodeSatker[2].".".$kodeSatker[3];
+
+					//NomorRegBaru
+					$sqlAsetNew = "SELECT noRegister FROM aset WHERE kodeKelompok = '{$value2['kodeKelompok']}' AND kodeLokasi = '{$kodeLokasi}' ORDER BY noRegister DESC LIMIT 1";
+					//echo "queryPA : ".$sqlAsetNew."\n\n";
+					$resultAsetNew = $link->query($sqlAsetNew);
+					$detailAsetNew = mysqli_fetch_assoc($resultAsetNew);
+					//print_r($detailAsetNew);
+					if($detailAsetNew['noRegister'] == ''){
+				        $startreg = 0; 
+				        $NomorRegBaru = $startreg + 1;
+				    }else{
+				    	$NomorRegBaru = intval($detailAsetNew['noRegister']) + 1;
+				    }
+    
+					//update mutasiaset			
+					$queryPA = "UPDATE mutasiaset SET Status = '1',NomorRegBaru = '{$NomorRegBaru}'
+								WHERE Mutasi_ID = '{$IDPenetapanMTS}' 
+										AND Aset_ID = '{$Aset_ID}'" 
+								or die("Error in the consult.." . mysqli_error($link));
+					$execPA = $link->query($queryPA);	
+					//echo "queryPA : ".$queryPA."\n\n";
+
+					$tgl_mutasi = "2017-12-29";
+					
+					//get all param to log
+					$sqlKIB = "SELECT * FROM {$table} where 
+								Aset_ID = '{$Aset_ID}'" 
+								or die("Error in the consult.." . mysqli_error($link));;
+					//echo "sqlKIB : ".$sqlKIB."\n\n";
+					$result = $link->query($sqlKIB); 
+					$ListParam = array();
+					while($rows = mysqli_fetch_assoc($result)) {
+						$ListParam = $rows;
+					} 
+					//print_r($ListParam);
+
+					// log pertama
+					$tmpField = array();
+					$tmpVal = array();
+					$sign = "'"; 	
+					foreach ($ListParam as $key3 => $val3) {
+						//print_r($ListParam);
+						//print_r($key);
+						//print_r($val);
+						$tmpField[] = $key3;
+						if ($val3 =='' || $val3 == NULL){
+							$tmpVal[] = 'NULL';
+						}else{
+							$tmpVal[] = $sign.addslashes($val3).$sign;
+						}
+					}
+
+					$implodeField = implode (',',$tmpField);
+					$implodeVal = implode (',',$tmpVal);
+
+					$AddField = "action,changeDate,TglPerubahan,Kd_Riwayat,NilaiPerolehan_Awal,No_Dokumen";
+					$action = "Data Mutasi sebelum diubah";
+					$changeDate = date('Y-m-d');
+					$TglPerubahan = $tgl_mutasi;
+					$Kd_Riwayat = '3';
+					$NilaiPerolehan_Awal = $ListParam['NilaiPerolehan'];
+					$No_Dokumen = $NoUsulan;
+					
+					//insert log
+					$QueryLog  = "INSERT INTO {$tableLog} ($implodeField,$AddField)
+									VALUES ($implodeVal,'$action','$changeDate','$TglPerubahan','$Kd_Riwayat','$NilaiPerolehan_Awal','$No_Dokumen')" or die("Error in the consult.." . mysqli_error($link));
+					//echo "QueryLog1 : ".$QueryLog."\n\n";
+					$execLog = $link->query($QueryLog);	
+					
+					//UPDATE DATA ASET DAN KIB
+					$quertAST = "UPDATE aset SET kodeLokasi = '{$kodeLokasi}' ,kodeSatker='{$SatkerTujuan}',noRegister = '{$NomorRegBaru}',TglPembukuan = '{$tgl_mutasi}'
+						WHERE Aset_ID = '{$Aset_ID}'" or die("Error in the consult.." . mysqli_error($link));	
+					$execAST = $link->query($quertAST);	
+					//echo "quertAST : ".$quertAST."\n\n";
+
+					//update tabel sqlKIB
+					$quertKIB = "UPDATE {$table} SET kodeLokasi = '{$kodeLokasi}' ,kodeSatker='{$SatkerTujuan}',noRegister = '{$NomorRegBaru}', TglPembukuan = '{$tgl_mutasi}'
+						WHERE Aset_ID = '{$Aset_ID}'" or die("Error in the consult.." . mysqli_error($link));	
+					$execKIB = $link->query($quertKIB);	
+					//echo "quertKIB : ".$quertKIB."\n\n";
+	
+					//log kedua
+					//get all param to log
+					$sqlKIB2 = "SELECT * FROM {$table} where 
+								Aset_ID = '{$Aset_ID}'" 
+								or die("Error in the consult.." . mysqli_error($link));;
+					//echo "sqlKIB : ".$sqlKIB."\n\n";
+					$result2 = $link->query($sqlKIB2); 
+					$ListParam2 = array();
+					while($rows2 = mysqli_fetch_assoc($result2)) {
+						$ListParam2 = $rows2;
+					}
+
+					// log kedua
+					$tmpField2 = array();
+					$tmpVal2 = array();
+					$sign2 = "'"; 	
+					foreach ($ListParam2 as $key4 => $val4) {
+						//print_r($ListParam);
+						//print_r($key);
+						//print_r($val);
+						$tmpField2[] = $key4;
+						if ($val4 =='' || $val4 == NULL){
+							$tmpVal2[] = 'NULL';
+						}else{
+							if($key4 == 'TglPembukuan'){
+								$tmpVal2[] = $sign2.addslashes($tgl_mutasi).$sign2;
+							}else{
+								$tmpVal2[] = $sign2.addslashes($val4).$sign2;			
+							}
+						}
+					}
+
+					$implodeField2 = implode (',',$tmpField2);
+					$implodeVal2 = implode (',',$tmpVal2);
+
+					$AddField2 = "action,changeDate,TglPerubahan,Kd_Riwayat,NilaiPerolehan_Awal,No_Dokumen";
+					$action2 = "Sukses Mutasi";
+					$changeDate2 = date('Y-m-d');
+					$TglPerubahan2 = $tgl_mutasi;
+					$Kd_Riwayat2 = '3';
+					$NilaiPerolehan_Awal2 = $ListParam2['NilaiPerolehan'];
+					$No_Dokumen2 = $NoUsulan;
+					
+					//insert log
+					$QueryLog2  = "INSERT INTO {$tableLog} ($implodeField2,$AddField2)
+									VALUES ($implodeVal2,'$action2','$changeDate2','$TglPerubahan2','$Kd_Riwayat2','$NilaiPerolehan_Awal2','$No_Dokumen2')" or die("Error in the consult.." . mysqli_error($link));
+					
+					$execLog2 = $link->query($QueryLog2);	
+					//echo "quertAST : ".$QueryLog2."\n\n"; 
+
 				$no++;
 				}
 			}
@@ -115,24 +277,39 @@ echo "SATKER AWAL : ".$rowKontrak['kodeSatker']."\n\n";
 			$DetailListAsset = implode(',', $listAsset);
 			echo "List Aset : ".$DetailListAsset."\n\n";
 			
-			//update usulan
-			$quertUS = "UPDATE usulan SET StatusPenetapan = '1' , Penetapan_ID = '{$IDPenetapanMTS}'
+			//update penetapan dan validasi pada usulan 
+			$quertUS = "UPDATE usulan SET StatusPenetapan = '1' , Penetapan_ID = '{$IDPenetapanMTS}',Status = '1'
 						WHERE Usulan_ID = '{$IDUsulan}'"
 						or die("Error in the consult.." . mysqli_error($link));  
 			//echo "quertUS : ".$quertUS."\n\n";			
 			$execUS = $link->query($quertUS);	
 
-			//update data usulan
-			$quertUSA = "UPDATE usulanaset SET Penetapan_ID='{$IDPenetapanMTS}', StatusPenetapan ='1', StatusKonfirmasi ='1'
+			//update data penetapan dan validasi usulan
+			$quertUSA = "UPDATE usulanaset SET Penetapan_ID='{$IDPenetapanMTS}', StatusPenetapan ='1', StatusKonfirmasi ='1',StatusValidasi='1'
 					WHERE Jenis_Usulan = 'MTS' AND Usulan_ID = '{$IDUsulan}'" or die("Error in the consult.." . mysqli_error($link));
 			//echo "quertUSA : ".$quertUSA."\n\n";				
 			$execUSA = $link->query($quertUSA);	
+
+			//UPDATE FixMutasi PENETAPAN MUTASI DI MUTASI(flag proses input data selesai) 
+			$queryMts = "UPDATE mutasi SET FixMutasi = '1'
+							WHERE Mutasi_ID = '{$IDPenetapanMTS}'" or die("Error in the consult.." . mysqli_error($link));
+			$execMts = $link->query($queryMts);	
+
 
 			echo "========================================================================================================="."\n\n";
 		$num++;		
 		}	
 	}
 
+$time_end = microtime(true);
+
+//dividing with 60 will give the execution time in minutes other wise seconds
+$execution_time = ($time_end - $time_start)/60;
+
+//execution time of the script
+echo "<b>Total Execution Time:</b> ".$execution_time." Mins"."\n\n";
+
+echo "=================== Process Complete. Thank you ===================\n\n";
 
 
 
