2d125025bb736b8d2cc1053372298ff59164f849
andre.hadiyono <andre.hadiyono@gmail.com>
2018-03-10 10:34:39 +0700

    revisi detail v3 untuk filtering data hapus

--- report/template/PEROLEHAN/rekapdetailrincianbarangmutasi.php
+++ report/template/PEROLEHAN/rekapdetailrincianbarangmutasi.php
@@ -16,8 +16,8 @@ $paramater_url="menuID=$modul&mode=$mode&tab=$tab&skpd_id=$skpd_id&levelAset=$le
 // echo $paramater_url;
 // exit;
 $REPORT=new report_engine();
-$url = "rekap_rincian_detail_v2.php?$paramater_url";
-//$url = "rekap_rincian_detail_v3.php?$paramater_url";
+//$url = "rekap_rincian_detail_v2.php?$paramater_url";
+$url = "rekap_rincian_detail_v3.php?$paramater_url";
 $REPORT->show_pilih_download_detail($url);  
 
 
