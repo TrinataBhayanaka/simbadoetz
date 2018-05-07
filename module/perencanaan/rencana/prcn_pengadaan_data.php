67dd1ae02616e8b66973cbfb37622aef31fb03a1
ilmawan <ilmawan.muharam@gmail.com>
2018-05-07 20:19:52 +0700

    update menuid

--- module/perencanaan/rencana/prcn_pengadaan_data.php
+++ module/perencanaan/rencana/prcn_pengadaan_data.php
@@ -2,7 +2,7 @@
 include "../../../config/config.php";
 
 	$PERENCANAAN = new RETRIEVE_PERENCANAAN;
-$menu_id = 10;
+$menu_id = 61;
             $SessionUser = $SESSION->get_session_user();
             ($SessionUser['ses_uid']!='') ? $Session = $SessionUser : $Session = $SESSION->get_session(array('title'=>'GuestMenu', 'ses_name'=>'menu_without_login')); 
             $USERAUTH->FrontEnd_check_akses_menu($menu_id, $Session);
--- module/perencanaan/rencana/prcn_pengadaan_filter.php
+++ module/perencanaan/rencana/prcn_pengadaan_filter.php
@@ -1,6 +1,6 @@
 <?php
 include "../../../config/config.php";
-$menu_id = 10;
+$menu_id = 61;
             $SessionUser = $SESSION->get_session_user();
             ($SessionUser['ses_uid']!='') ? $Session = $SessionUser : $Session = $SESSION->get_session(array('title'=>'GuestMenu', 'ses_name'=>'menu_without_login')); 
             $USERAUTH->FrontEnd_check_akses_menu($menu_id, $Session);
